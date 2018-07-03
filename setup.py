6a204efe51903636dacd1d582ff70db8cdde7f8f
Demian <dmyanster@gmail.com>
2018-07-03 17:51:11 -0300

    testing prj C

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=['projectc'],
 
     # # Needed for dependencies
-    install_requires=['test_pkg_B'],
+    install_requires=['PackageB'],
     dependency_links=[
         # # 'git+https://github.com/dmyanster/test_pkg_A.git@v{}#egg=PackageA.egg-info'.format('0.0.4'),
         # # 'git+https://github.com/dmyanster/test_pkg_B.git@v{}#egg=PackageB.egg-info'.format('0.0.1'),
