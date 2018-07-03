2e3542818f9b7c3e32c1d94b56db60ce97d1b2e9
Demian <dmyanster@gmail.com>
2018-07-03 16:32:28 -0300

    testing prj C

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     # # Needed for dependencies
     install_requires=['test_pkg_B'],
     dependency_links=[
-        'git+https://github.com/dmyanster/test_pkg_A.git@v$1#egg=PackageA.egg-info',
-        'git+https://github.com/dmyanster/test_pkg_B.git@v$1#egg=PackageB.egg-info',
+        'git+https://github.com/dmyanster/test_pkg_A.git@v{}#egg=PackageA.egg-info'.format('0.0.4'),
+        'git+https://github.com/dmyanster/test_pkg_B.git@v{}#egg=PackageB.egg-info'.format('0.0.1'),
     ],
 
     # *strongly* suggested for sharing
