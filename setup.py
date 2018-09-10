3bdc66f5d2b682a6e1252a4d2ed1c8ae0e4f8c23
Demian <dmyanster@gmail.com>
2018-09-10 16:38:07 -0300

    testing prj C - v0.0.3

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 
 def install_deps():
-    """Reads requirements.txt and preprocess it
+    """Reads requirements_pip.txt and preprocess it
     to be feed into setuptools.
 
     This is the only possible way (we found)
-    how requirements.txt can be reused in setup.py
+    how requirements_pip.txt can be reused in setup.py
     using dependencies from private github repositories.
 
     Links must be appendend by `-{StringWithAtLeastOneNumber}`
@@ -21,7 +21,7 @@ def install_deps():
     Returns:
          list of packages and dependency links.
     """
-    default = open('requirements.txt', 'r').readlines()
+    default = open('requirements_pip.txt', 'r').readlines()
     new_pkgs = []
     links = []
     for resource in default:
@@ -62,7 +62,7 @@ setup(
     # ],
 
     # *strongly* suggested for sharing
-    version='0.0.2',
+    version='0.0.3',
 
     license='private test code',
     description='any description',
